2227cc25411d2e9419796b6c721165600e80c0c1
B0hr <trenson.gilles@gmail.com>
2020-02-14 01:36:01 +0100

    Added GPX preview

--- src/index.js
+++ src/index.js
@@ -1,11 +1,83 @@
-console.log('Hello world')
+import { map, LatLng } from 'leaflet'
+import 'leaflet-draw'
 
-window.onload = function(){
+window.onload = () => {
 
-    console.log('qslkdfjm')
+    const mapElement = document.getElementById('map')
 
-    console.log('test')
+    const map = L.map(mapElement).setView([51.505, -0.09], 13)
 
-    console.log('qmsldkj')
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
+    // Definieer een tekengroep
+    const drawnItems = new L.FeatureGroup()
+    drawnItems.addTo(map)
+    // Definieer de tools
+    const drawControl = new L.Control.Draw({
+        draw: {
+            polygon: false,
+            marker: false,
+            circlemarker: false
+        },
+        edit: {
+            featureGroup: drawnItems
+        }
+    })
+    drawControl.addTo(map)
+    // Wanneer een nieuwe tekening is aangemaakt
+    map.on('draw:created', (e) => {
+        const { layerType, layer } = e
+        // Voeg toe aan de kaart
+        layer.addTo(map)
+    })
+
+    const preventDefault = e => {
+        // Voorkom event listeners van parent elements
+        e.stopPropagation()
+        e.preventDefault()
+    }
+
+    mapElement.addEventListener('dragover', (e) => {
+        preventDefault(e)
+    })
+
+    mapElement.addEventListener('dragenter', (e) => {
+        preventDefault(e)
+    })
+
+    mapElement.addEventListener('drop', (e) => {
+        preventDefault(e)
+
+        let dt = e.dataTransfer
+        let files = dt.files
+
+        for (let file of files){
+            file.text().then(text => {
+                const parser = new DOMParser()
+                const doc = parser.parseFromString(text, 'application/xml')
+                // Haal alle segmenten op
+                const segments = Array.from(doc.getElementsByTagName('trkseg'))
+                for (let segment of segments){
+                    const points = Array.from(segment.getElementsByTagName('trkpt'))
+                    // Zet punten op de kaart
+                    const pointsSubset = points.filter((e, i) => {
+                        return i % 1000
+                    })
+                    // Ga verder met de subset
+                    for (let point of pointsSubset){
+                        if ('lat' in point.attributes && 'lon' in point.attributes){
+                            const lat = point.getAttribute('lat')
+                            const lon = point.getAttribute('lon')
+                            L.marker([lat, lon]).addTo(map)
+                        }
+                    }
+                    const lat = points[0].getAttribute('lat')
+                    const lon = points[0].getAttribute('lon')
+                    map.flyTo([lat, lon])
+                }
+            })
+
+            console.log(`Name: ${file.name} - ${file.size}`)
+        }
+    })
 
 }
