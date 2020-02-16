eb872985fdfdab79f02f90bdf707d6e24fcf37af
B0hr <trenson.gilles@gmail.com>
2020-02-16 13:56:17 +0100

    use mapbox-gl-js instead of leaflets

--- src/index.js
+++ src/index.js
@@ -1,34 +1,24 @@
-import { map, LatLng } from 'leaflet'
-import 'leaflet-draw'
+import { Map, NavigationControl } from 'mapbox-gl'
+import { lineString, lineDistance, point, along } from '@turf/turf'
 
 window.onload = () => {
 
-    const mapElement = document.getElementById('map')
+    // Instellingen mapbox
+    const accessToken = 'pk.eyJ1IjoicmVib3QiLCJhIjoiY2sxMTE2bzNjMDA1ODNlcDU5NHQ3c29oMiJ9.VfDPJP1o62zvc8GzywbRbg'
 
-    const map = L.map(mapElement).setView([51.505, -0.09], 13)
+    // Maak de map aan
+    const map = new Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [2, 50],
+        zoom: 9,
+        hash: false,
+        interactive: false,
+        accessToken
+    })
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
-    // Definieer een tekengroep
-    const drawnItems = new L.FeatureGroup()
-    drawnItems.addTo(map)
-    // Definieer de tools
-    const drawControl = new L.Control.Draw({
-        draw: {
-            polygon: false,
-            marker: false,
-            circlemarker: false
-        },
-        edit: {
-            featureGroup: drawnItems
-        }
-    })
-    drawControl.addTo(map)
-    // Wanneer een nieuwe tekening is aangemaakt
-    map.on('draw:created', (e) => {
-        const { layerType, layer } = e
-        // Voeg toe aan de kaart
-        layer.addTo(map)
-    })
+    // Voeg zoom en rotatie knoppen toe
+    map.addControl(new NavigationControl())
 
     const preventDefault = e => {
         // Voorkom event listeners van parent elements
@@ -36,15 +26,17 @@ window.onload = () => {
         e.preventDefault()
     }
 
-    mapElement.addEventListener('dragover', (e) => {
+    const kaart = document.getElementById('map')
+
+    kaart.addEventListener('dragover', (e) => {
         preventDefault(e)
     })
 
-    mapElement.addEventListener('dragenter', (e) => {
+    kaart.addEventListener('dragenter', (e) => {
         preventDefault(e)
     })
 
-    mapElement.addEventListener('drop', (e) => {
+    kaart.addEventListener('drop', (e) => {
         preventDefault(e)
 
         let dt = e.dataTransfer
@@ -68,18 +60,87 @@ window.onload = () => {
                         if ('lat' in point.attributes && 'lon' in point.attributes){
                             const lat = point.getAttribute('lat')
                             const lon = point.getAttribute('lon')
-                            latlngs.push([lat, lon])
+                            latlngs.push([lon, lat])
                         }
                     }
-                    L.polyline(latlngs, {color: 'red'}).addTo(map)
-                    const lat = points[0].getAttribute('lat')
-                    const lon = points[0].getAttribute('lon')
-                    map.flyTo([lat, lon])
+                    // Algemeen
+                    const options = {units: 'kilometers'}
+                    // Maak een GeoJson aan
+                    const route = lineString(latlngs, {
+                        width: 2,
+                        color: '#C62828'
+                    })
+                    const routeLengte = lineDistance(route, options)
+                    // Maak de huidige marker aan
+                    const huidigeLocatie = along(route, 0, options)
+                    // Voeg route toe als bron
+                    map.addSource('route', {
+                        'type': 'geojson',
+                        'data': {
+                            'type': 'FeatureCollection',
+                            'features': [
+                                route
+                            ]
+                        }
+                    })
+                    map.addSource('point', {
+                        'type': 'geojson',
+                        'data': {
+                            'type': 'FeatureCollection',
+                            'features': [
+                                huidigeLocatie
+                            ]
+                        }
+                    })
+                    // Toon de route 
+                    map.addLayer({
+                        'id': 'route',
+                        'type': 'line',
+                        'source': 'route',
+                        'layout': {
+                            'line-join': 'round',
+                            'line-cap': 'round'
+                        },
+                        'paint': {
+                            'line-color': ['get', 'color'],
+                            'line-width': ['get', 'width']
+                        }
+                    })
+                    map.addLayer({
+                        'id': 'point',
+                        'source': 'point',
+                        'type': 'symbol',
+                        'layout': {
+                            'icon-image': 'bicycle-15'
+                        }
+                    })
+                    console.log(route)
+                    // Vlieg naar de start van de route
+                    map.jumpTo({
+                        center: huidigeLocatie.geometry.coordinates,
+                        essential: true
+                    })
+                    
+                    let i = 0
+                    let timer = window.setInterval(() => {
+                        if (i < routeLengte) {
+                            const nieuweLocatie = along(route, i, options)
+                            map.getSource('point').setData(nieuweLocatie)
+                            map.panTo(nieuweLocatie.geometry.coordinates)
+                            i += 0.1
+                        } else {
+                            window.clearInterval(timer)
+                        }
+                    }, 10)
                 }
             })
 
             console.log(`Name: ${file.name} - ${file.size}`)
         }
     })
+
+/*     window.onscroll = () => {
+
+    } */
 
 }
