0443cf289cb3f7c41f3ef9d6a067fbd0d7595e1c
B0hr <trenson.gilles@gmail.com>
2020-03-01 22:28:41 +0100

    Updated folder structure - still need to check if everything works

--- src/js/config/index.js
+++ src/js/config/index.js
@@ -1,8 +1,15 @@
 // AWS Services om de gpx'en op te slaan
 // --> AWS S3 Bucket
 module.exports = {
-    aws: {
-        accessKeyId: process.env.AWS_PUBLIC_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY 
-    }
+  aws: {
+    // User profile - only for debugging purpose
+    accessKeyId: process.env.AWS_PUBLIC_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    // Identity pool - to configure unauth and auth requests
+    identityPoolId: process.env.AWS_COGNITO_IDENTITY_POOL
+  },
+  mapbox: {
+    // Mapbox Access Token
+    accessToken: process.env.MAPBOX_ACCESS_TOKEN
+  }
 }
--- src/js/services/aws.js
+++ src/js/services/aws.js
@@ -1,20 +1,59 @@
-import { aws } from './../config'
+import {
+    aws as awsConfig
+} from './../config'
 import S3 from 'aws-sdk/clients/s3'
+import AWS from 'aws-sdk'
 
+AWS.config.region = 'eu-central-1'
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: awsConfig.identityPoolId
+})
+
+/** Make a S3 Bucket instance */
 const s3bucket = new S3({
-    // Authenticatie
-    accessKeyId: aws.accessKeyId,
-    secretAccessKey: aws.secretAccessKey,
     params: {
-        // Bind de bucket toe aan de klasse
-        // Nu moet deze niet meer herhaald worden
-        // voor elke api call
+        /**
+         * Attach the bucket name to the 
+         * S3 object as a default so we 
+         * shouldn't repeat it in our calls
+         */
         Bucket: 'rebot-travelogue'
     },
     // Huidige api versie
     apiVersion: '2006-03-01'
 })
 
+/**
+ * Upload a File object to the s3bucket
+ *
+ * @param {file} f - File object
+ */
+const s3bucketUpload = async f => {
+    const params = {
+        Key: f.name,
+        ContentType: f.type,
+        Body: f,
+        ACL: 'public-read'
+    }
+    /** Upload the file to the S3 Bucket */
+    const data = await s3bucket.putObject(params).promise()
+}
+
+/**
+ * Retrieve a list of Files from the S3 Bucket
+ *
+ * @param {number} [size=10] - Amount of files to retrieve
+ */
+const s3bucketList = async (size=10) => {
+    const params = {
+        MaxKeys: size
+    }
+    /** Retrieve the last 10 files which the user can view */
+    const fileList = await s3bucket.listObjectsV2(params).promise()
+}
+
 module.exports = {
-    s3bucket
+  s3bucket,
+  s3bucketUpload,
+  s3bucketList
 }
