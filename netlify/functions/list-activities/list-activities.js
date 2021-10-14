d2f9d63569c90b5c207cdc749a544126dd63eee9
anton <antondejaeger@waylay.io>
2021-10-14 22:39:35 +0200

    Added cloud functions

---
+++ netlify/functions/list-activities/list-activities.js
@@ -0,0 +1,42 @@
+const fetch = require('node-fetch')
+
+const username = process.env.WAYLAY_CLIENT
+const password = process.env.WAYLAY_SECRET
+
+const handler = async function () {
+  try {
+    const response = await fetch('https://api-io.waylay.io/storage/v1/bucket/assets/traveloque/GPX/', {
+      headers: {
+        Accept: 'application/json',
+        Authorization: 'Basic ' + Buffer.from(username + ':' + password).toString('base64')
+      }
+    })
+    if (!response.ok) {
+      // NOT res.status >= 200 && res.status < 300
+      return { statusCode: response.status, body: response.statusText }
+    }
+    const data = await response.json()
+    const files = data.objects.map(file => {
+      return {
+        name: file.name.replace('traveloque/GPX/',''),
+        lastModified: file.last_modified
+      }
+    })
+
+    return {
+      statusCode: 200,
+      body: files
+    }
+  } catch (error) {
+    // output to netlify function log
+    console.log(error)
+    return {
+      statusCode: 500,
+      // Could be a custom message or object i.e. JSON.stringify(err)
+      body: JSON.stringify({ msg: error.message }),
+    }
+  }
+}
+
+handler()
+module.exports = { handler }
---
+++ netlify/functions/upload-activity/upload-activity.js
@@ -0,0 +1,41 @@
+const fetch = require('node-fetch')
+
+const username = process.env.WAYLAY_CLIENT
+const password = process.env.WAYLAY_SECRET
+
+const handler = async function (event) {
+  try {
+    const body = JSON.parse(event.body)
+    const { fileName, fileType } = body
+
+    if (!fileName && !fileType) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: 'Missing fileName or fileType on body'
+        })
+      }
+    }
+
+    if (fileType === 'application/xml') {
+      // upload gpx
+
+    }
+    if (fileType === 'photo') {
+      // create folder with gpx name
+
+      // upload photos in gpx name folder
+    }
+
+  } catch (error) {
+    // output to netlify function log
+    console.log(error)
+    return {
+      statusCode: 500,
+      // Could be a custom message or object i.e. JSON.stringify(err)
+      body: JSON.stringify({ msg: error.message }),
+    }
+  }
+}
+
+module.exports = { handler }
